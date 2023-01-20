4858902957a39da6c05ab0aeeb6816395dc6face
Shivakumar-R-T <112469085+Shivakumar-R-T@users.noreply.github.com>
2023-01-20 21:20:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [relatives, setRelatives] = useState(['Prasanna', 'Manjula', 'Divya', 'Sandhya'])
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol>
+        {relatives.map((relative,idx) => { 
+       return   <li key={relative+idx}> Name: {relative}</li>
+        })}
+      </ol>
+
     </div>
   )
 }
